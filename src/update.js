f46a67859bad0ffe503229123e863b73c7179163
Richard Shade <131337+rshade@users.noreply.github.com>
2020-10-26 05:09:51 -0500

    fix: fix issue with update breaking if data folder doesn't exist (#24)

--- src/update.js
+++ src/update.js
@@ -1,3 +1,10 @@
 const updaters = require('./updaters.js');
+var fs = require('fs');
+var path = require('path')
+var dir = path.join(__dirname, '../data')
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
 updaters.run();
