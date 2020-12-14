8249f0d80ad89d3cbb356b04ef1c3bf0c1b38f52
danielmabbett <15195905+DanielMabbett@users.noreply.github.com>
2020-12-14 03:20:17 +0000

    Update dockerfiles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,30 @@ set -e
 
 ## Run startup command 
 echo "Running Start Up Script__"
-npm run update -- --only=azure:retail
+
+if [[ -z "${CLOUD_PROVIDER}" ]]; then
+  UPDATE_PARAM="All"
+else
+  UPDATE_PARAM="${CLOUD_PROVIDER}"
+fi
+
+if [ "${UPDATE_PARAM}" == "All" ]; then
+	npm run update
+fi
+
+if [ "${UPDATE_PARAM}" == "AWS" ]; then
+	npm run update -- --only=aws:bulk
+	npm run update -- --only=aws:spot
+fi
+
+if [ "${UPDATE_PARAM}" == "Azure" ]; then
+	npm run update -- --only=azure:retail
+fi
+
+if [ "${UPDATE_PARAM}" == "GCP" ]; then
+	npm run update -- --only=gcp:catalog
+	npm run update -- --only=gcp:machineTypes
+fi
 
 ## Running passed command
 if [[ "$1" ]]; then
