76ddd6145d986b3531c531777c739a5189da0ad1
danielmabbett <15195905+DanielMabbett@users.noreply.github.com>
2021-03-16 11:43:46 +0000

    add UPDATE_CLOUD_PROVIDER environment setting option add services to single infracost network default UPDATE_CLOUD_PROVIDER is none

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 ## Run startup command 
 echo "Running Start Up Script__"
 
-case $UPDATE_PARAM in
+case $UPDATE_CLOUD_PROVIDER in
 
   "All")
 	  npm run update
     ;;
 
-  "AWS")
+  "AWS" | "Amazon" | "AmazonWebServices")
     npm run update -- --only=aws:bulk
     npm run update -- --only=aws:spot
     ;;
