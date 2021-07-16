82699b6505add55dadb9041d9e5cd97e15194533
tim775 <52185+tim775@users.noreply.github.com>
2021-07-15 22:47:17 -0400

    feat: add dump command

---
+++ src/cmd/dump.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import config from '../config';
+import dumper from '../dumper/run';
+
+const dir = path.join(__dirname, '../data/products');
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+async function run() {
+  await dumper.run();
+}
+
+run()
+  .then(() => {
+    config.logger.info('dump complete');
+    process.exit(0);
+  })
+  .catch((err) => {
+    config.logger.error(err);
+    process.exit(1);
+  });
