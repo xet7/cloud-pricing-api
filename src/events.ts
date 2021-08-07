054d9cee4df9c3b77cd3ff8c732c50261e8a6498
Alistair Scott <ali.scott@gmail.com>
2021-08-07 17:18:07 +0100

    refactor: move forward events to its own function

--- src/events.ts
+++ src/events.ts
@@ -7,7 +7,6 @@ import { incrementCounters } from './stats/stats';
 const router = express.Router();
 
 router.post('/event', async (req, res) => {
-  // Store the counters
   if (req.body.event === 'infracost-run') {
     const isCi = !!req.body?.env?.ciPlatform;
     const installId = req.body?.env?.installId;
@@ -15,27 +14,40 @@ router.post('/event', async (req, res) => {
     incrementCounters(isCi, installId);
   }
 
-  // Forward events to Dashboard
-  if (!config.disableTelemetry && req.body.event === 'infracost-run') {
-    const attrs = ['ciPlatform', 'ciScript', 'fullVersion', 'installId'];
-
-    const body = {
-      event: req.body.event,
-      env: {
-        ..._.pick(req.body.env, attrs),
-        isSelfHosted: true,
-      },
-    };
-
-    await axios.post(`${config.infracostDashboardApiEndpoint}/event`, body, {
-      headers: {
-        'X-Api-Key': config.infracostAPIKey,
-        'X-Cloud-Pricing-Api-Version': process.env.npm_package_version,
-      },
-    });
+  if (!config.disableTelemetry) {
+    await forwardEvent(req.body?.event || '', req.body?.env);
   }
 
   return res.json({ status: 'ok' });
 });
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+async function forwardEvent(event: string, env: any): Promise<void> {
+  // Only forward run events
+  if (event !== 'infracost-run') {
+    return;
+  }
+
+  // Only forward the following attributes
+  const attrs = ['ciPlatform', 'ciScript', 'fullVersion', 'installId'];
+
+  const body = {
+    event,
+    env: {
+      ..._.pick(env, attrs),
+      isSelfHosted: true,
+    },
+  };
+
+  await axios.post(`${config.infracostDashboardApiEndpoint}/event`, body, {
+    headers: {
+      'X-Api-Key': config.infracostAPIKey,
+      'X-Cloud-Pricing-Api-Version': process.env.npm_package_version,
+    },
+  });
+}
+/* eslint-enable @typescript-eslint/no-explicit-any */
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
 
 export default router;
